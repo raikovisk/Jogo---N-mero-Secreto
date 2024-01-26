a70362a2d74032e150cba21237d8252ad082e3f1
Raisson <58261944+raikovisk@users.noreply.github.com>
2024-01-26 16:08:51 +0000

    Atualizando sorteio dos números

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+let numMax = 10;
+let numSorteados = [];
 let secretNumber = geraNum();
 let tentativas = 0;
 console.log("Número:" + secretNumber);
@@ -8,28 +10,48 @@ function escreveMsg( tag, msg ){
 }
 
 function geraNum(){
-    return parseInt(Math.random() * 10) + 1;
+    let numSort = parseInt(Math.random() * numMax + 1);
+    if(numSorteados.includes(numSort)){
+      numSort = parseInt(Math.random() * numMax + 1);
+    }else{
+      numSorteados.push(numSort);
+      return numSort;
+    }
+    console.log(numSorteados);
+}
+
+function limpaCampo (){
+  chute.value = '';
+}
+
+function resetaJogo(){
+  secretNumber = geraNum();
+  limpaCampo();
+  tentativas = 0;
+  escreveMsg('#titulo', 'Adivinhe o numero secreto');
+  escreveMsg('#paragrafo', `Escolha um número entre 1 a ${numMax}`);
 }
 
 function verificaChute(){
-    let chute = document.querySelector("#chute").value;
+    let chute = document.querySelector("#chute");
     tentativas++;
-  if (chute == secretNumber){
+
+  if (chute.value == secretNumber){
     escreveMsg('h1', `Acertou! O número secreto é ${secretNumber}`);
     if(tentativas => 1){
       escreveMsg('p', `Em ${tentativas} Tentativa `);
     }else{
       escreveMsg('p', `Em ${tentativas} Tentativas `);
     }
+    //Habilita o botão Novo Jogo
+    document.getElementById('reiniciar').removeAttribute('disabled');
   }else{
-      if (chute < secretNumber){
-        escreveMsg('p', `O número secreto é maior!`);
-      }else{
-        escreveMsg('p', `O número secreto é menor!`);
-      }
+    limpaCampo();
+    if (chute.value < secretNumber){
+      escreveMsg('p', `O número secreto é maior!`);
+    }else{
+      escreveMsg('p', `O número secreto é menor!`);
     }
-}
+  }
 
-function resetaJogo(){
-  
 }
